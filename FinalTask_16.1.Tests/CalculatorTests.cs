a32b87fc9a5f96b150e09eae3ad4e3d3abb69e6f
Роман Толмачёв <unifiq@gmail.com>
2021-03-22 23:26:03 +0300

    fin

---
+++ FinalTask_16.1.Tests/CalculatorTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace FinalTask_16._1.Tests
+{
+	[TestFixture]
+	class CalculatorTests
+	{
+		[Test]
+		public void AdditionalMustReturnCorrectValue()
+		{
+			Calculator calc = new Calculator();
+			Assert.That(calc.Additional(220, 330) == 550);
+		}
+
+		[Test]
+		public void SubtractionMustReturnCorrectValue()
+		{
+			Calculator calc = new Calculator();
+			Assert.That(calc.Subtraction(330, 220) == 110);
+		}
+
+		[Test]
+		public void MiltiplicationMustReturnCorrectValue()
+		{
+			Calculator calc = new Calculator();
+			Assert.That(calc.Miltiplication(12, 34) == 408);
+		}
+		
+
+		[Test]
+		public void DivisionMustReturnCorrectValue()
+		{
+			Calculator calc = new Calculator();
+			Assert.That(calc.Division(100, 10) == 10);
+		}
+
+		[Test]
+		public void DivisionMustThrowException()
+		{
+			Calculator calc = new Calculator();
+			Assert.Throws<DivideByZeroException>(() => calc.Division(30, 0));
+		}
+	}
+}
